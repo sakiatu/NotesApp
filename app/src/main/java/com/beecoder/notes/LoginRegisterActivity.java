a7e3d32468306f7b0f7a1bc017faac4c265c2084
sakib20 <sakib20@iut-dhaka.edu>
2020-06-01 16:05:21 +0600

    complete Authentication process

--- app/src/main/java/com/beecoder/notes/LoginRegisterActivity.java
+++ app/src/main/java/com/beecoder/notes/LoginRegisterActivity.java
@@ -2,22 +2,70 @@ package com.beecoder.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginRegisterActivity extends AppCompatActivity {
-    private Button loginOrRegister;
+    private static final String TAG = "LoginRegisterActivity";
+    private static final int AUTH_UI_RC = 1010;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
 
-        loginOrRegister = findViewById(R.id.login_reg);
-        loginOrRegister.setOnClickListener(v->login());
+        findViewById(R.id.login_reg).setOnClickListener(v -> handleLogin());
     }
 
-    private void login() {
+    private void handleLogin() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build()
+        );
 
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers).build();
+        startActivityForResult(intent, AUTH_UI_RC);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == AUTH_UI_RC) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()) {
+                    Toast.makeText(this, "Welcome New User", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Welcome back Again", Toast.LENGTH_SHORT).show();
+                }
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            } else {
+                if (response == null) {
+                    Log.i(TAG, "onActivityResult: user cancelled sign in");
+                } else {
+                    Log.i(TAG, "onActivityResult: " + response.getError());
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/beecoder/notes/MainActivity.java
+++ app/src/main/java/com/beecoder/notes/MainActivity.java
@@ -3,20 +3,33 @@ package com.beecoder.notes;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
     private RecyclerView recyclerView;
     private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
 
         fab = findViewById(R.id.fab);
+    }
+
+    private void openLoginActivity() {
+        startActivity(new Intent(this, LoginRegisterActivity.class));
+        finish();
     }
 
     @Override
@@ -55,6 +73,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void logOut() {
+        AuthUI.getInstance().signOut(this);
+    }
 
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        //if user sign out then this method will be called and user should be null
+        if (firebaseAuth.getCurrentUser() == null) {
+            openLoginActivity();
+            return;
+        }
+        firebaseAuth.getCurrentUser().getIdToken(true)
+                .addOnSuccessListener(getTokenResult -> {
+                    Log.i("token", "onAuthStateChanged: " + getTokenResult.getToken());
+                });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FirebaseAuth.getInstance().removeAuthStateListener(this);
     }
 }
